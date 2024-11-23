3bcf31dbb900149869d85e6c8600bc72fa44b53e
Sebastian-Alexandru Popa <alexsebastian2006@yahoo.com>
2024-11-23 20:19:16 +0200

    Initial Commit

---
+++ TrackBites/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TrackBites
+//
+//  Created by Sebastian-Alexandru Popa on 23.11.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TrackBites/TrackBitesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrackBitesApp.swift
+//  TrackBites
+//
+//  Created by Sebastian-Alexandru Popa on 23.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct TrackBitesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
